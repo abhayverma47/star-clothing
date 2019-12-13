75ba587024683b9bd764add765c9f20a54dd1681
abhayverma47 <vabhay2012@gmail.com>
2019-12-13 13:10:28 +0530

    images and components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.components'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+
+    <div>
+      <Switch>
+        <Route exact={true} path='/' component={HomePage} />
+      </Switch>
     </div>
+
   );
 }
 
 export default App;
+
+ //switch maps first path and dosent map anything after it
+
+ //exact by default is true
